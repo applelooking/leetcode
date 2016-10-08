7a890b7e8656b022346d7e342af66b3271166bff
applelooking <gys_921020@163.com>
2016-10-08 16:41:04 -0700

    finish 227

---
+++ 227.cpp
@@ -0,0 +1,17 @@
+// Forward declaration of the knows API.
+bool knows(int a, int b);
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int celebrity = 0;
+        for (int i = 1;i < n;++i) {
+            if (knows(celebrity, i)) celebrity = i;
+        }
+        for (int i = 0;i < n;++i) {
+            if (celebrity == i) continue;
+            if (knows(celebrity, i) || !knows(i, celebrity)) return -1;
+        }
+        return celebrity;
+    }
+};

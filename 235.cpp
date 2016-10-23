232e6b3d033fe78748b8a4296e5c0bb4318dd826
applelooking <gys_921020@163.com>
2016-10-23 15:28:38 -0700

    easy question

---
+++ 235.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while (root) {
+            if ((root->val - p->val) * (root->val - q->val) <= 0) {
+                return root;
+            } else {
+                if (root->val > p->val) root = root->left;
+                else root = root->right;
+            }
+        }
+        return root;
+    }
+};

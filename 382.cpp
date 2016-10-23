b5b1987511a0122dc0458670d80e05313a391cab
applelooking <gys_921020@163.com>
2016-10-23 15:13:50 -0700

    finish reservoir sampling problem, really interesting

---
+++ 382.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    ListNode* head;
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int result = head->val;
+        int i = 2;
+        ListNode* ptr = head->next;
+        while (ptr) {
+            int tmp = rand() % i;
+            if (tmp == 0) {
+                result = ptr->val;
+            }
+            ++i;
+            ptr = ptr->next;
+        }
+        return result;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */

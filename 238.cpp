e598cc302fb8e9db2948eb5cb8ced7a35cb14bec
applelooking <gys_921020@163.com>
2016-10-24 10:59:55 -0700

    optimize the previous solution

--- 238.cpp
+++ 238.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> result;
-        unordered_map<int, int> right;
-        if (nums.empty()) return result;
-        int product = 1;
-        for (int i = nums.size() - 1;i >= 0;--i) {
-            right[i] = product;
-            product *= nums[i];
+        if (nums.empty()) return vector<int>();
+        vector<int> result(nums.size(), 1);
+        int cur = nums[0];
+        for (int i = 1;i < nums.size();++i) {
+            result[i] *= cur;
+            cur *= nums[i];
         }
-        product = 1;
-        for (int i = 0; i < nums.size();++i) {
-            result.push_back(product * right[i]);
-            product *= nums[i];
+        cur = nums.back();
+        for (int i = nums.size() - 2;i >= 0;--i) {
+            result[i] *= cur;
+            cur *= nums[i];
         }
         return result;
     }
